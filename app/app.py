5b2b1e969fa31e5dac0ee3de8811e52d2ce97388
SHARANA BASAVA <137386052+sharanabasava-dev@users.noreply.github.com>
2026-03-13 22:34:47 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Sharan 🚀 DevOps Project Running Successfully!"
+    return "Hello Sharan 🚀 DevOps Project Running Successfully! v2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
